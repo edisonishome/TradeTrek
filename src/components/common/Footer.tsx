b28cda709aab99bdc21e4e15fc492ce4c1313a8c
edisonishome <nhantrong581@gmail.com>
2024-10-20 14:31:40 +0700

    update contact

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,24 +2,15 @@ const Footer = () => {
   return (
     <footer className="w-full bg-gray-800 py-4 relative" data-testid="page-footer">
       <div className="contaoned mx-auto px-4 text-center text-gray-200">
-        Mark Arenz &copy;{new Date().getFullYear()}
+        Trọng Nhân &copy;{new Date().getFullYear()}
         <span> | </span>
         <a
-          href="https://www.ridiculopathy.com"
+          href="https://www.facebook.com/edisonnothome"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-orange-300 hover:underline transition-all duration-150"
         >
-          More Projects
-        </a>
-        <span> | </span>
-        <a
-          href="https://www.markmakesstuff.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hover:text-orange-300 hover:underline transition-all duration-150"
-        >
-          Blog
+          Facebook
         </a>
       </div>
     </footer>
